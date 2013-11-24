29e5c3b563caaf630fc32c1ad39b4eb5f4f9f2d5
Jack Hoy <jackhoy05@gmail.com>
2013-11-24 14:51:21 +0000

    refactoring tests to use aStar module

--- graph_search/aStar/src/aStar.js
+++ graph_search/aStar/src/aStar.js
@@ -1,73 +1,82 @@
-// Helper methods
-function forEach(array, action) {
-    for (var i = 0; i < array.length; i++) {
-        action(array[i]);
-    }
-}
+_ = require('underscore');
 
-function calculateGCost(currentCoordinates, currentCoordinatesID, closedList) {
-    var previousGCost = closedList[((currentCoordinatesID) - 1)].gCost;
-    var currentMovementCost = 10;
-    return previousGCost + currentMovementCost;
-}
+// module.exports.calculateGCost = function (currentLocation, currentLocation, closedList) {
+//     var previousGCost = closedList[((currentLocation) - 1)].gCost;
+//     var currentMovementCost = 10;
+//     return previousGCost + currentMovementCost;
+// };
 
-// calculates estimated distance to the destination co-ordinates from current co-ordinates in absolute terms, ignoring diagonal moves and obstacles
-function calculateHCost(currentCoordinates, destinationCoordinates) {
-    return (Math.abs(destinationCoordinates["x"] - currentCoordinates["x"]) * 10) + (Math.abs(destinationCoordinates["y"] - currentCoordinates["y"]) * 10);
-}
+// calculates estimated distance to the destination co-ordinates from current co-ordinates in absolute terms,
+// ignoring diagonal moves and obstacles
+module.exports.calculateHCost = function (currentLocation, destination) {
+    return (Math.abs(destination["x"] - currentLocation["x"]) * 10) +
+           (Math.abs(destination["y"] - currentLocation["y"]) * 10);
+};
 
-function calculateFCost(currentCoordinates) {
-    return (currentCoordinates.hCost + currentCoordinates.gCost);
-}
+module.exports.getAdjacentCoordinates = function (currentLocation) {
+    adjacentMovements = [{x: -1, y: 1}, {x: 0, y: 1}, {x: 1, y: 1},
+                         {x: -1, y: 0}, {x: 1, y: 0},
+                         {x: -1, y: -1}, {x: 0, y: -1}, {x: 1, y: -1}];
+    adjacentCoordinates = _.map(adjacentMovements, function (movement) {
+        movement.x += this.x;
+        movement.y += this.y;
+        return movement;
+    }, currentLocation);
+    return adjacentCoordinates;
+};
 
-// returns {point} from the [openList] with the lowest fCost 
-function findPointWithLowestFCost(openList) {
-    var pointWithLowestFCost = null;
-    forEach(openList, function (element) {
-        if (pointWithLowestFCost === null) {
-            pointWithLowestFCost = element;
-        }
-        else if (element.fCost < pointWithLowestFCost.fCost) {
-            pointWithLowestFCost = element;
-        }
-    });
-    return pointWithLowestFCost;
-}
+// module.exports.calculateFCost = function (currentLocation) {
+//     return (currentLocation.hCost + currentLocation.gCost);
+// };
 
-// receives an array of coordinates hash data [{coordinates1}, {coordinates2}, {...} ]
-// returns true if a coordinate is within the bounds of the world and is not the coordinate of a wall
-function validateCoordinates(coordinates, worldData) {
-    var coordinatesValid = true;
-    if (coordinates["x"] > worldData["xBoundary"] || coordinates["y"] > worldData["yBoundary"]) {
-        coordinatesValid = false;
-    }
-    forEach(worldData["walls"], function (element) {
-        if (coordinates["x"] == element["x"] && coordinates["y"] == element["y"]) {
-            coordinatesValid = false;
-        }
-    });
-    return coordinatesValid;
-}
+// // returns {point} from the [openList] with the lowest fCost
+// module.exports.findPointWithLowestFCost = function (openList) {
+//     var pointWithLowestFCost = null;
+//     forEach(openList, function (element) {
+//         if (pointWithLowestFCost === null) {
+//             pointWithLowestFCost = element;
+//         }
+//         else if (element.fCost < pointWithLowestFCost.fCost) {
+//             pointWithLowestFCost = element;
+//         }
+//     });
+//     return pointWithLowestFCost;
+// };
 
-function generateAdjacentCoordinates(currentCoordinates, worldData) {
-    var validMovements = [];
-    var movements = [{x: 0, y: 1, cost: 10, direction: "north"},
-                     {x: 1, y: 1, cost: 14, direction: "northEast"},
-                     {x: 1, y: 0, cost: 10, direction: "east"},
-                     {x: 1, y: -1, cost: 14, direction: "southEast"},
-                     {x: 0, y: -1, cost: 10, direction: "south"},
-                     {x: -1, y: -1, cost: 14, direction: "southWest"},
-                     {x: -1, y: 0, cost: 10, direction: "west"},
-                     {x: -1, y: 1, cost: 14, direction: "northWest"}];
-    forEach(movements, function (element) {
-        element["x"] += currentCoordinates["x"];
-        element["y"] += currentCoordinates["y"];
-        if (validateCoordinates(element, worldData) === true) {
-            validMovements.push(element);
-        }
-    });
-    return validMovements;
-}
+// // receives an array of coordinates hash data [{coordinates1}, {coordinates2}, {...} ]
+// // returns true if a coordinate is within the bounds of the world and is not the coordinate of a wall
+// module.exports.validateCoordinates = function (coordinates, worldData) {
+//     var coordinatesValid = true;
+//     if (coordinates["x"] > worldData["xBoundary"] || coordinates["y"] > worldData["yBoundary"]) {
+//         coordinatesValid = false;
+//     }
+//     forEach(worldData["walls"], function (element) {
+//         if (coordinates["x"] == element["x"] && coordinates["y"] == element["y"]) {
+//             coordinatesValid = false;
+//         }
+//     });
+//     return coordinatesValid;
+// };
+
+// module.exports.generateAdjacentCoordinates = function (currentLocation, worldData) {
+//     var validMovements = [];
+//     var movements = [{x: 0, y: 1, cost: 10, direction: "north"},
+//                      {x: 1, y: 1, cost: 14, direction: "northEast"},
+//                      {x: 1, y: 0, cost: 10, direction: "east"},
+//                      {x: 1, y: -1, cost: 14, direction: "southEast"},
+//                      {x: 0, y: -1, cost: 10, direction: "south"},
+//                      {x: -1, y: -1, cost: 14, direction: "southWest"},
+//                      {x: -1, y: 0, cost: 10, direction: "west"},
+//                      {x: -1, y: 1, cost: 14, direction: "northWest"}];
+//     forEach(movements, function (element) {
+//         element["x"] += currentLocation["x"];
+//         element["y"] += currentLocation["y"];
+//         if (validateCoordinates(element, worldData) === true) {
+//             validMovements.push(element);
+//         }
+//     });
+//     return validMovements;
+// };
 
 
 
@@ -78,18 +87,18 @@ function generateAdjacentCoordinates(currentCoordinates, worldData) {
 //   var startPoint = {id: "0"
 //                    ,parent: "0"
 //                    ,x: startCoordinates["x"]
-//                    ,y: startCoordinates["y"] 
+//                    ,y: startCoordinates["y"]
 //                    ,gCost: 0
-//                    ,hCost: calculateHCost(startCoordinates, destinationCoordinates)  
+//                    ,hCost: calculateHCost(startCoordinates, destinationCoordinates)
 //   };
 //   startPoint["fCost"] = calculateFCost(startPoint);
 //   openList.push(startPoint);
 
   // loop
   // var nextToExplore = nextfindPointWithLowestFCost(openList);
-  // move nextToExplore it to the closedList 
+  // move nextToExplore it to the closedList
   // remove nextToExplore from the open list
-  // for each adjacent square to the nextToExplore 
+  // for each adjacent square to the nextToExplore
   //   if not on the openlist, add it with nextToExplore as the parent, record f,g,h costs of the adjacent squares
   //   else if its already on the openlist check to see if path to this square has a better G cost
   //     if it is better
@@ -97,7 +106,7 @@ function generateAdjacentCoordinates(currentCoordinates, worldData) {
   // stop if you find the destination square or the open list is empty
 
   // calculate the path by going from the closed list - then following the parents backwards.
- 
+
 // }
 
 

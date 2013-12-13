cff79016a10d7a112ed075378c324415e8a707a6
Jack Hoy <jackhoy05@gmail.com>
2013-12-13 14:48:47 +0000

    refactor searchFor function

--- graph_search/aStar/src/aStar.js
+++ graph_search/aStar/src/aStar.js
@@ -14,52 +14,51 @@ exports.getAdjacentCoordinates = function (currentLocation) {
 };
 
 // calculates the movement cost from the start to the current location based on the path generated to get there.
-// exports.calculateGCost = function (currentLocation, currentLocation, closedList) {
-//     var previousGCost = closedList[((currentLocation) - 1)].gCost;
-//     var currentMovementCost = 10;
-//     return previousGCost + currentMovementCost;
-// };
+exports.calculateGCost = function (currentLocation, currentLocation, closedList) {
+    var previousGCost = closedList[((currentLocation) - 1)].gCost;
+    var currentMovementCost = 10;
+    return previousGCost + currentMovementCost;
+};
 
 // calculates estimated distance to the destination co-ordinates from current co-ordinates in absolute terms,
 // ignoring diagonal moves and obstacles
 exports.calculateHCost = function (currentLocation, destination) {
-    return (Math.abs(destination["x"] - currentLocation["x"]) * 10) +
-           (Math.abs(destination["y"] - currentLocation["y"]) * 10);
+    return (Math.abs(destination.x - currentLocation.x) * 10) +
+           (Math.abs(destination.y - currentLocation.y) * 10);
 };
 
-// exports.calculateFCost = function (currentLocation) {
-//     return (currentLocation.hCost + currentLocation.gCost);
-// };
+exports.calculateFCost = function (currentLocation) {
+    return (currentLocation.hCost + currentLocation.gCost);
+};
 
-
-// // returns {point} from the [openList] with the lowest fCost
-// exports.findPointWithLowestFCost = function (openList) {
-//     var pointWithLowestFCost = null;
-//     forEach(openList, function (element) {
-//         if (pointWithLowestFCost === null) {
-//             pointWithLowestFCost = element;
-//         }
-//         else if (element.fCost < pointWithLowestFCost.fCost) {
-//             pointWithLowestFCost = element;
-//         }
-//     });
-//     return pointWithLowestFCost;
-// };
+// returns {point} from the [openList] with the lowest fCost
+exports.findPointWithLowestFCost = function (openList) {
+    var pointWithLowestFCost = null;
+    forEach(openList, function (element) {
+        if (pointWithLowestFCost === null) {
+            pointWithLowestFCost = element;
+        }
+        else if (element.fCost < pointWithLowestFCost.fCost) {
+            pointWithLowestFCost = element;
+        }
+    });
+    return pointWithLowestFCost;
+};
 
 // // receives an array of coordinates hash data [{coordinates1}, {coordinates2}, {...} ]
 // // returns true if a coordinate is within the bounds of the world and is not the coordinate of a wall
-// exports.validateCoordinates = function (coordinates, worldData) {
-//     var coordinatesValid = true;
-//     if (coordinates["x"] > worldData["xBoundary"] || coordinates["y"] > worldData["yBoundary"]) {
-//         coordinatesValid = false;
-//     }
-//     forEach(worldData["walls"], function (element) {
-//         if (coordinates["x"] == element["x"] && coordinates["y"] == element["y"]) {
-//             coordinatesValid = false;
-//         }
-//     });
-//     return coordinatesValid;
-// };
+exports.validateCoordinates = function (coordinates, worldData) {
+    var coordinatesValid = true;
+    if (coordinates["x"] > worldData["xBoundary"] || coordinates["y"] > worldData["yBoundary"]) {
+        coordinatesValid = false;
+    }
+    forEach(worldData["walls"], function (element) {
+        if (coordinates["x"] == element["x"] && coordinates["y"] == element["y"]) {
+            coordinatesValid = false;
+        }
+    });
+    return coordinatesValid;
+};
 
 
 exports.searchFor = function (destination, startLocation, environment) {
@@ -72,42 +71,14 @@ exports.searchFor = function (destination, startLocation, environment) {
   idCounter += 1;
   openList += start;
 
-  // add starting point to open list of squares to be considered.
-  // choose square from the open list with lowest fCost. Remove from open list and add to closed list
-  // generate legal adjacent co-ordinates from the chosen square and add them to the openlist with the chosen square as parentSquare, costs
-
-  // repeat
-
-};
-
-//   };
-//   var openList = [];
-//   var closedList = [];
-//   var startPoint = {id: "0"
-//                    ,parent: "0"
-//                    ,x: startCoordinates["x"]
-//                    ,y: startCoordinates["y"]
-//                    ,gCost: 0
-//                    ,hCost: calculateHCost(startCoordinates, destinationCoordinates)
-//   };
-//   startPoint["fCost"] = calculateFCost(startPoint);
-//   openList.push(startPoint);
-
   // loop
   // var nextToExplore = nextfindPointWithLowestFCost(openList);
-  // move nextToExplore it to the closedList
+  // move nextToExplore to the closedList
   // remove nextToExplore from the open list
   // for each adjacent square to the nextToExplore
-  //   if not on the openlist, add it with nextToExplore as the parent, record f,g,h costs of the adjacent squares
+  //   if not on the openlist, add it with nextToExplore as the parent, record f,g,h costs of the adjacent squares, add id
   //   else if its already on the openlist check to see if path to this square has a better G cost
   //     if it is better
-  //        change the parent of this square to the current square - recalcualte g and f costs of that square
+  //        change the parent of this square to the current square - recalculate g and f costs of that square
   // stop if you find the destination square or the open list is empty
-
-  // calculate the path by going from the closed list - then following the parents backwards.
-
-// }
-
-
-
-
+};

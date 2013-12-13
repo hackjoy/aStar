13ab25af6713e1b9507a263b49b02cb957078802
Jack Hoy <jackhoy05@gmail.com>
2013-12-13 14:59:11 +0000

    renamed x and y properties to xAxis / yAxis

--- graph_search/aStar/src/aStar.js
+++ graph_search/aStar/src/aStar.js
@@ -2,12 +2,12 @@ _ = require('underscore');
 
 // generates adjacent x,y coordinates. Passed a current location e.g. {x: 2 y: 4}, returns an array of 8 x,y coordinate objects
 exports.getAdjacentCoordinates = function (currentLocation) {
-    adjacentMovements = [{x: -1, y: 1}, {x: 0, y: 1}, {x: 1, y: 1},
-                         {x: -1, y: 0}, {x: 1, y: 0},
-                         {x: -1, y: -1}, {x: 0, y: -1}, {x: 1, y: -1}];
+    adjacentMovements = [{xAxis: -1, yAxis: 1}, {xAxis: 0, yAxis: 1}, {xAxis: 1, yAxis: 1},
+                         {xAxis: -1, yAxis: 0}, {xAxis: 1, yAxis: 0},
+                         {xAxis: -1, yAxis: -1}, {xAxis: 0, yAxis: -1}, {xAxis: 1, yAxis: -1}];
     adjacentCoordinates = _.map(adjacentMovements, function (movement) {
-        movement.x += this.x;
-        movement.y += this.y;
+        movement.xAxis += this.xAxis;
+        movement.yAxis += this.yAxis;
         return movement;
     }, currentLocation);
     return adjacentCoordinates;
@@ -23,8 +23,8 @@ exports.calculateGCost = function (currentLocation, currentLocation, closedList)
 // calculates estimated distance to the destination co-ordinates from current co-ordinates in absolute terms,
 // ignoring diagonal moves and obstacles
 exports.calculateHCost = function (currentLocation, destination) {
-    return (Math.abs(destination.x - currentLocation.x) * 10) +
-           (Math.abs(destination.y - currentLocation.y) * 10);
+    return (Math.abs(destination.xAxis - currentLocation.xAxis) * 10) +
+           (Math.abs(destination.yAxis - currentLocation.yAxis) * 10);
 };
 
 exports.calculateFCost = function (currentLocation) {
@@ -49,36 +49,35 @@ exports.findPointWithLowestFCost = function (openList) {
 // // returns true if a coordinate is within the bounds of the world and is not the coordinate of a wall
 exports.validateCoordinates = function (coordinates, worldData) {
     var coordinatesValid = true;
-    if (coordinates["x"] > worldData["xBoundary"] || coordinates["y"] > worldData["yBoundary"]) {
+    if (coordinates.xAxis > worldData.xBoundary || coordinates.yAxis > worldData.yBoundary) {
         coordinatesValid = false;
     }
-    forEach(worldData["walls"], function (element) {
-        if (coordinates["x"] == element["x"] && coordinates["y"] == element["y"]) {
+    forEach(worldData.walls, function (element) {
+        if (coordinates.xAxis == element.xAxis && coordinates.yAxis == element.yAxis) {
             coordinatesValid = false;
         }
     });
     return coordinatesValid;
 };
 
-
 exports.searchFor = function (destination, startLocation, environment) {
-  var openList = [];
-  var closedList = [];
-  var idCounter = 0;
+    var openList = [];
+    var closedList = [];
+    var idCounter = 0;
 
-  // prepare the startLocation as the first square in openList
-  start = {id: idCounter, x: startLocation.x, y: startLocation.y, parentSquare: idCounter, fCost: 0, gCost: 0, hCost: 0};
-  idCounter += 1;
-  openList += start;
+    // prepare the startLocation as the first square in openList
+    start = {id: idCounter, xAxis: startLocation.xAxis, yAxis: startLocation.yAxis, parentSquare: idCounter, fCost: 0, gCost: 0, hCost: 0};
+    idCounter += 1;
+    openList += start;
 
-  // loop
-  // var nextToExplore = nextfindPointWithLowestFCost(openList);
-  // move nextToExplore to the closedList
-  // remove nextToExplore from the open list
-  // for each adjacent square to the nextToExplore
-  //   if not on the openlist, add it with nextToExplore as the parent, record f,g,h costs of the adjacent squares, add id
-  //   else if its already on the openlist check to see if path to this square has a better G cost
-  //     if it is better
-  //        change the parent of this square to the current square - recalculate g and f costs of that square
-  // stop if you find the destination square or the open list is empty
+    // loop
+    // var nextToExplore = nextfindPointWithLowestFCost(openList);
+    // move nextToExplore to the closedList
+    // remove nextToExplore from the open list
+    // for each adjacent square to the nextToExplore
+    //   if not on the openlist, add it with nextToExplore as the parent, record f,g,h costs of the adjacent squares, add id
+    //   else if its already on the openlist check to see if path to this square has a better G cost
+    //     if it is better
+    //        change the parent of this square to the current square - recalculate g and f costs of that square
+    // stop if you find the destination square or the open list is empty
 };
